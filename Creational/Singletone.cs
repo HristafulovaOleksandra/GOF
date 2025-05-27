b9adc636a1605b58b91864869b276f1021c6d78d
HristafulovaOleksandra <hristafulovaoleksandra@gmail.com>
2025-05-27 15:14:04 +0300

    SingletoneExample

---
+++ Creational/Singletone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOF_Lab2.Creational
+{
+    public class Singletone
+    {
+        private static Singletone singletone = null;
+
+        Singletone()
+        { }
+
+        public static Singletone getInstance()
+        {
+            if (singletone == null)
+                singletone = new Singletone();
+            return singletone;
+        }
+
+        public void catMeow()
+        {
+            System.Console.WriteLine("MEOW");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GOF_Lab2.Creational
+{ 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Singletone singletone =Singletone.getInstance();
+            singletone.catMeow();
+        }
+    }
+}
